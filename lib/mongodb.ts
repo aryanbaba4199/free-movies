77eb37ec1e5df01163d72707c3bf9addd18aebb5
Aryan Dubey <aryanbaba@gmail.com>
2023-11-20 13:47:05 +0530

    ndd

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const dburi : any = process.env.DB;
+  const dburi = "mongodb://your-mongodb-uri"; // Replace this with your actual MongoDB URI
   try {
-    const conn = await mongoose.connect(dburi);
+    const conn = await mongoose.connect(dburi, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Add other options if needed
+    });
 
-    console.log(`Connected to Database : `);
+    console.log(`Connected to Database`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error}`);
   }
