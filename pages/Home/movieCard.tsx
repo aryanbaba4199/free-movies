dc6ba7c557066334d57bef82e39e75d3c42ac9dd
Aryan Dubey <aryanbaba@gmail.com>
2023-11-20 13:11:25 +0530

    jhjhs

--- pages/Home/movieCard.tsx
+++ pages/Home/movieCard.tsx
@@ -1,31 +1,38 @@
-// MovieCard.js
-import React from 'react';
-import {useRouter} from "next/router"
-const MovieCard = ({movie} : any) => {
-  const { name, image, imdbRating, releaseDate,} = movie;
-  const id : any = movie._id;
-  const router = useRouter();
+// // MovieCard.js
+// import React from 'react';
+// import {useRouter} from "next/router"
+// const MovieCard = ({movie} : any) => {
+//   const { name, image, imdbRating, releaseDate,} = movie;
+//   const id : any = movie._id;
+//   const router = useRouter();
 
-  const movieLink = () => {
-    router.push(`/movieSelected?id=${id}`);
-  }
+//   const movieLink = () => {
+//     router.push(`/movieSelected?id=${id}`);
+//   }
   
-  return (
-    <>
-    <div className="mx-auto bg-white shadow-md overflow-hidden rounded-md w-72 h-80 hover:cursor-pointer" onClick={movieLink}>
-      <img className="w-full h-48 object-cover" src={image} alt={image} />
-      <div className="p-6">
-        <h2 className="text-xl font-semibold text-gray-800">Download {name} full Movie</h2>
-        <p className="text-gray-600 mb-4">Released on: {releaseDate}</p>
-        <p className="text-gray-700">IMDb Rating: {imdbRating}</p>
-      </div>
+//   return (
+//     <>
+//     <div className="mx-auto bg-white shadow-md overflow-hidden rounded-md w-72 h-80 hover:cursor-pointer" onClick={movieLink}>
+//       <img className="w-full h-48 object-cover" src={image} alt={image} />
+//       <div className="p-6">
+//         <h2 className="text-xl font-semibold text-gray-800">Download {name} full Movie</h2>
+//         <p className="text-gray-600 mb-4">Released on: {releaseDate}</p>
+//         <p className="text-gray-700">IMDb Rating: {imdbRating}</p>
+//       </div>
       
-    </div>
+//     </div>
     
-    </>
-  );
-};
+//     </>
+//   );
+// };
 
-export default MovieCard;
+// export default MovieCard;
 
+import React from 'react'
+
+export default function movieCard() {
+  return (
+    <div>movieCard</div>
+  )
+}
 
