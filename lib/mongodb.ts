80708150c7685c8eb74bbe60a32c2ae369784c8c
Aryan Dubey <aryanbaba@gmail.com>
2023-11-20 13:49:02 +0530

    ihd

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,11 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   const dburi = "mongodb://your-mongodb-uri"; // Replace this with your actual MongoDB URI
   try {
-    const conn = await mongoose.connect(dburi, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // Add other options if needed
-    });
+    const conn = await mongoose.connect("mongodb+srv://aryanbaba4199:Aryan7277@cluster0.j3sovlh.mongodb.net/?retryWrites=true&w=majority")
 
     console.log(`Connected to Database`);
   } catch (error) {
